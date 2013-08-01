8bac2e18a259992c0078f3a12c0c867d635edd7b
lunatik-210 <andrew.d.lapin@gmail.com>
2013-08-01 18:45:03 +0400

    Removed invalid lines

--- gles2.0/PaintingView.cs
+++ gles2.0/PaintingView.cs
@@ -46,48 +46,7 @@ namespace AndroidUI {
             "        v_normal=(uNormal*vec4(n_normal,1.0)).xyz;" +
             "        gl_Position = uProjection * uView * vertex;" +
             "}";
-        /*
-                private string fragmentShaderCode =
-                    "precision mediump float;" +
-                    "const int MAX_LIGHTS = 8;" +
-                    "struct Light " +
-                    "{" +
-                    "    vec3 position;" +
-                    "    vec4 color;" +
-                    "    vec3 attenuation;" +
-                    "};" +
-                    "uniform Light u_lights[MAX_LIGHTS];" +
-                    "uniform int numLights;" +
-                    "uniform vec3 u_camera;" +
-                    "varying vec3 v_vertex;" +
-                    "varying vec3 v_normal;" +
-                    "void main() {" +
-                    "        vec3 n_normal=normalize(v_normal);" +
-                    "        vec3 lightvector;" +
-                    "        vec3 lookvector = normalize(u_camera - v_vertex);" +
-                    "        vec3 dirvector = u_lights[i].position - v_vertex;" +
-                    "        float ambient = 0.2;" +
-                    "        float k_diffuse = 0.8;" +
-                    "        float k_specular = 0.8;" +
-                    "        vec4 final_color = vec4(0.0, 0.0, 0.0, 0.0);" +
-                    "        float diffuse;" +
-                    "        vec3 reflectvector;" +
-                    "        float specular;" +
-                    "        float attenuation;" +
-        //            "        float distance = length(dirvector);" + 
-                    "        for(int i=0; i<MAX_LIGHTS; i++) {" +
-                    "            if( i >= numLights )" +
-                    "                break;" +
-                    "            attenuation = 1.0 / (u_lights[i].attenuation[0] + u_lights[i].attenuation[1]*distance + u_lights[i].attenuation[2]*distance*distance); " +
-                    "            lightvector = normalize(dirvector);" +
-                    "            diffuse = k_diffuse * max(dot(n_normal, lightvector), 0.0);" +
-                    "            reflectvector = reflect(-lightvector, n_normal);" +
-                    "            specular = k_specular * pow(max(dot(lookvector, reflectvector), 0.0), 40.0);" +
-                    "            final_color += (ambient+diffuse+specular)*u_lights[i].color;" +
-                    "        }" +
-                    "        gl_FragColor = final_color;" +
-                    "}";
-         */
+
         private string fragmentShaderCode =
             "precision mediump float;" +
             "const int MAX_LIGHTS = 8;" +
