72c16ab98a825cf1fb36c0d7a11fd3aa9290bd28
lunatik-210 <andrew.d.lapin@gmail.com>
2013-08-05 14:37:09 +0400

    shader refactoring

--- gles2.0/Assets/Shaders/sh.fs
+++ gles2.0/Assets/Shaders/sh.fs
@@ -10,6 +10,7 @@ struct Light
 };
 
 uniform Light u_lights[MAX_LIGHTS];
+
 uniform int numLights;
 uniform vec3 u_camera;
 
@@ -17,30 +18,32 @@ varying vec3 v_vertex;
 varying vec3 v_normal;
 
 void main() {
-        vec3 n_normal=normalize(v_normal);
-        vec3 lookvector = normalize(u_camera - v_vertex);
-        vec3 lightvector;
-        vec3 dirvector;
-        float ambient = 0.2;
-        float k_diffuse = 0.8;
-        float k_specular = 0.8;
-        vec4 final_color = vec4(0.0, 0.0, 0.0, 0.0);
-        float diffuse;
-        vec3 reflectvector;
-        float specular;
-        float distance;
-        float attenuation;
-        for(int i=0; i<MAX_LIGHTS; i++) {
-            if( i >= numLights )
-                break;
-            dirvector = u_lights[i].position - v_vertex;
-            distance = length(dirvector);
-            lightvector = normalize(dirvector);
-            attenuation = 1.0 / (u_lights[i].attenuation[0] + u_lights[i].attenuation[1]*distance + u_lights[i].attenuation[2]*distance*distance);
-            diffuse = k_diffuse * max(dot(n_normal, lightvector), 0.0);
-            reflectvector = reflect(-lightvector, n_normal);
-            specular = k_specular * pow(max(dot(lookvector, reflectvector), 0.0), 40.0);
-            final_color += (ambient+diffuse+specular)*attenuation*u_lights[i].color;
-        }
-        gl_FragColor = final_color;
+    float diffuse, specular, distance, attenuation;
+    vec3 lightvector, dirvector, reflectvector;
+    float ambient = 0.2, k_diffuse = 0.8, k_specular = 0.8;
+
+    vec3 n_normal=normalize(v_normal);
+    vec3 lookvector = normalize(u_camera - v_vertex);
+    vec4 final_color = vec4(0.0, 0.0, 0.0, 0.0);
+
+    for(int i=0; i<MAX_LIGHTS; i++) {
+        if( i >= numLights )
+            break;
+
+        dirvector = u_lights[i].position - v_vertex;
+
+        distance = length(dirvector);
+        lightvector = normalize(dirvector);
+
+        attenuation = 1.0 / (u_lights[i].attenuation[0] + u_lights[i].attenuation[1]*distance + u_lights[i].attenuation[2]*distance*distance);
+
+        diffuse = k_diffuse * max(dot(n_normal, lightvector), 0.0);
+
+        reflectvector = reflect(-lightvector, n_normal);
+
+        specular = k_specular * pow(max(dot(lookvector, reflectvector), 0.0), 40.0);
+
+        final_color += (ambient+diffuse+specular)*attenuation*u_lights[i].color;
+    }
+    gl_FragColor = final_color;
 }
--- gles2.0/Assets/Shaders/sh.vs
+++ gles2.0/Assets/Shaders/sh.vs
@@ -10,9 +10,9 @@ varying vec3 v_vertex;
 varying vec3 v_normal;
 
 void main() {
-        vec4 vertex = uModel * vec4(a_vertex, 1.0);
-        v_vertex=vertex.xyz;
-        vec3 n_normal=normalize(a_normal);
-        v_normal=(uNormal*vec4(n_normal,1.0)).xyz;
-        gl_Position = uProjection * uView * vertex;
+	vec4 vertex = uModel * vec4(a_vertex, 1.0);
+	v_vertex=vertex.xyz;
+	vec3 n_normal=normalize(a_normal);
+	v_normal=(uNormal*vec4(n_normal,1.0)).xyz;
+	gl_Position = uProjection * uView * vertex;
 }
