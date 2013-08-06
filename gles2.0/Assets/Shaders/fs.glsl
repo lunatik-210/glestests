92ab0de155a67368efa4dd0d84aa534d5c1000bf
lunatik-210 <andrew.d.lapin@gmail.com>
2013-08-06 12:59:18 +0400

    Removed refract and reflect functions from shaders as they are already existing

--- gles2.0/Assets/Shaders/fs.glsl
+++ gles2.0/Assets/Shaders/fs.glsl
@@ -17,30 +17,6 @@ uniform vec3 u_camera;
 varying vec3 v_vertex;
 varying vec3 v_normal;
 
-void refract(inout vec3 ray, vec3 normal) 
-{   
-    
-    float eta = 1.0/0.3; // eta = in_IOR/out_IOR
-    
-    float cos_theta = -dot(normal, ray);
-
-    vec3 m_normal = vec3(normal);
-
-    if(cos_theta < 0.0)
-    {
-        cos_theta *= -1.0;
-        m_normal *= -1.0;
-        eta = 1.0/eta;
-    }
-
-    
-    float k = 1.0 - eta*eta*(1.0-cos_theta*cos_theta);
-    if(k >= 0.0)
-        ray = normalize( eta*ray + (eta*cos_theta - sqrt(k))*normal);
-    
-}
-
-
 void main() {
     float diffuse, specular, distance, attenuation;
     vec3 lightvector, dirvector, reflectvector;
@@ -56,7 +32,6 @@ void main() {
         
         // additional parameters
         dirvector = u_lights[i].position - v_vertex;
-        //refract(dirvector, n_normal);
         distance = length(dirvector);
         lightvector = normalize(dirvector);
 
